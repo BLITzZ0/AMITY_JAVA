5126c234202dcd1421c8810e23caefc76498e9e6
Abhishek Pandey <82567424+BLITzZ0@users.noreply.github.com>
2024-01-08 23:24:13 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+import Airthmetic.*;
+public class Main {
+    public static void main(String[]args){
+        Add a=new Add();
+        Sub b = new Sub();
+        System.out.println(b.sub(10,5));
+        System.out.println(a.add(10,5));
+    }
+}
